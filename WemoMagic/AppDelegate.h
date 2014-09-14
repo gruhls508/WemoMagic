c568226ddc109bbe149074f5fab722c374474b30
Glen Ruhl <glen.ruhl.90@gmail.com>
2014-09-14 09:39:26 -0500

    Initial Commit

---
+++ WemoMagic/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WemoMagic
+//
+//  Created by Glen Ruhl on 9/14/14.
+//  Copyright (c) 2014 MM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WemoMagic/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WemoMagic
+//
+//  Created by Glen Ruhl on 9/14/14.
+//  Copyright (c) 2014 MM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
